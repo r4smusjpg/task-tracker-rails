0cfd306135c83c434b0a2a1d582c883fa9c87ef4
R4smuS-jpg <r4smusjpg@gmail.com>
2022-06-12 16:08:05 +0300

    fix sign out

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :projects, :tasks
   resources :users, only: %i[show new create edit update]
 
-  resource :session, only: %i[new create destroy]
+  resource :session, only: %i[new create]
+  get '/sign-out', to: 'sessions#destroy'
+
 
   root 'projects#index'
 end
