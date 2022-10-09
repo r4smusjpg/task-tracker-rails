c78ec6297c78bb39332dda3d4aa651fb4a3b9374
R4smuS-jpg <r4smusjpg@gmail.com>
2022-10-09 12:54:26 +0300

    add destroy mutations for projects, tasks and comments

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -10,13 +10,16 @@ module Types
     # projects mutations
     field :create_project, mutation: Mutations::CreateProject
     field :update_project, mutation: Mutations::UpdateProject
+    field :destroy_project, mutation: Mutations::DestroyProject
 
     # tasks mutations
     field :create_task, mutation: Mutations::CreateTask
     field :update_task, mutation: Mutations::UpdateTask
+    field :destroy_task, mutation: Mutations::DestroyTask
 
     # comments mutations
     field :create_comment, mutation: Mutations::CreateComment
     field :update_comment, mutation: Mutations::UpdateComment
+    field :destroy_comment, mutation: Mutations::DestroyComment
   end
 end
