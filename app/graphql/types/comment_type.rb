5686691ee12855dcb530f1674ec7eee2dcdba730
R4smuS-jpg <r4smusjpg@gmail.com>
2022-10-22 00:20:10 +0300

    add task field to comment type

--- app/graphql/types/comment_type.rb
+++ app/graphql/types/comment_type.rb
@@ -3,9 +3,9 @@
 module Types
   class CommentType < Types::BaseObject
     field :id, ID, null: false
-    field :user_id, Integer
-    field :task_id, Integer
-    field :content, String
+    field :user_id, Integer, null: false
+    field :task, Types::TaskType, null: false
+    field :content, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
